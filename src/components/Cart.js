92adb1f7798d62824289942636dcfa1178ec73b6
Poe <pwas123@gmail.com>
2022-10-19 18:24:58 -0400

    Animated sidebar for shopping cart, need to fix fadeout transition and add cart functionality

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,30 +1,31 @@
 import './cart.css'
 
-const Cart = () => {
+const Cart = (props) => {
 
-
+    let backDrop;
+    let cart;
+    if(props.isOpen){
+        backDrop = 'cartBackDrop-open'
+        cart = 'cart-open'
+    }
+    else{
+        backDrop = 'cartBackDrop-close'
+        cart = 'cart-close'
+    }
 
     return(
-    <div id="cd-cart">
-        <h2>Cart</h2>
-        <ul class="cd-cart-items">
-            <li>
-            
-            </li>
+        <div>
+            <div className={backDrop} onClick={props.toggleCart}></div>
+            <div className={cart}>
+                <h2>Cart</h2>
 
-            <li>
-            
-            </li>
-        </ul> 
+                <div className="cd-cart-total">
+                    <p>Total <span>$39.96</span></p>
+                </div> 
 
-        <div class="cd-cart-total">
-            <p>Total <span>$39.96</span></p>
-        </div> 
-
-        <a href="#0" class="checkout-btn">Checkout</a>
-
-        <p class="cd-go-to-cart"><a href="#0">Go to cart page</a></p>
-    </div> 
+                <a href="#0" className="checkout-btn">Checkout</a>
+            </div> 
+        </div>
     )
 }
 
